762475d27f9f667c05d66eb08f31cb4d12f18c9a
dchulax <tobiasorao@gmail.com>
2018-12-02 19:01:14 +0300

    Add if at the moment new measures are feed

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
-const this.a = a;
-const this.b = b;
-const this.c = c;
-
-const this.aSquared =  a*a;
-const this.bSquared = b*b;
-const this.cSquared = c*c;
-
-$const = triangle.(document)(() => {
-  getElementByClass.submit(function(event) {
-   var fieldOne = parseInt($("input#field-one").val('Enter only numbers'));
-   var fieldTwo = parseInt($("input#field-two").val('Enter only numbers'));
-   var fieldThree = parseInt($("input#field-three").val('Enter only numbers'));
-   if (field(fieldOne, fieldTwo, fieldThree){
-
-   })
-
-}));
+$(document)(() => {
+     $form#fields.submit((event) {
+       var fieldOne = parseInt($("input#field-one").val('Enter only numbers'));
+       var fieldTwo = parseInt($("input#field-two").val('Enter only numbers'));
+       var fieldThree = parseInt($("input#field-three").val('Enter only numbers'));
+       const triangles = (a,b,c) =>{
+         if (this instanceof triangles){
+           const this.a = a;
+           const this.b = b;
+           const this.c = c;
+         }else{
+           return new triangles[];
+         }
+       }
+     });
+});
