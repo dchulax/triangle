af47d6d9e58b6e345501a5b4096d4f8b1bfac1d3
dchulax <tobiasorao@gmail.com>
2018-12-02 17:40:34 +0300

    Add jquery and modified styles.css and index.html

---
+++ app.js
@@ -0,0 +1,21 @@
+
+  var a = (c, b) => {
+    return c - b;
+  };
+
+  var b = (c, a) => {
+    return c - a;
+  };
+
+  var c = (a, b) => {
+    return a + b;
+  };
+}
+$const = triangle.(document)(() => {a
+  getElementByClass.submit(function(event) {
+   var fieldOne = parseInt($("input#field-one").val());
+   var fieldTwo = parseInt($("input#field-two").val());
+   var fieldThree = parseInt($("input#field-three").val());
+   if (field())
+
+}));
